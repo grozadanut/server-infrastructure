80d7195c3f4179292c28d7236bdd30a7da711f69
Groza Danut <groza_danut@yahoo.com>
2024-08-05 16:37:45 +0300

    Add docker community

--- scripts/ansible-galaxy-install-roles.sh
+++ scripts/ansible-galaxy-install-roles.sh
@@ -3,3 +3,4 @@
 ansible-galaxy install -r ../ansible/requirements.yml
 ansible-galaxy install --force git+https://github.com/grozadanut/ansible-role-seaweedfs.git,main
 ansible-galaxy install --force git+https://github.com/grozadanut/ansible-dockerswarm.git,master
+ansible-galaxy collection install community.docker
