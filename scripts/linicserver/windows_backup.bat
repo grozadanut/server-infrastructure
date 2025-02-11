fa6c753ba5a95bb76ab8e2f18913807ff9e31cd3
Groza Danut <groza_danut@yahoo.com>
2025-02-11 10:57:42 +0200

    Update windows_backup.bat add moqui database

--- scripts/linicserver/windows_backup.bat
+++ scripts/linicserver/windows_backup.bat
@@ -10,12 +10,15 @@ SET LINICSERVERPASS=[pass]
 "C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres -Z6 colibri_stat_db > D:\work\backup\colibri_stat_db_%dateformat%.gz
 "C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres -Z6 cloud-anaf-connector > D:\work\backup\cloud-anaf-connector_%dateformat%.gz
 "C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres -Z6 cloud-product-synchronizer > D:\work\backup\cloud-product-synchronizer_%dateformat%.gz
+"C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres -Z6 moqui > D:\work\backup\moqui_%dateformat%.gz
 
 D:
 cd D:\work\PuTTY
 set from1=D:\work\backup\colibri_stat_db_%dateformat%.gz
 set from2=D:\work\backup\cloud-anaf-connector_%dateformat%.gz
 set from3=D:\work\backup\cloud-product-synchronizer_%dateformat%.gz
+set from4=D:\work\backup\moqui_%dateformat%.gz
 pscp -pw %LINICSERVERPASS% %from1% linicserver@colibriserver.go.ro:/mnt/D/linicserver_backup_safe/
 pscp -pw %LINICSERVERPASS% %from2% linicserver@colibriserver.go.ro:/mnt/D/linicserver_backup_safe/
 pscp -pw %LINICSERVERPASS% %from3% linicserver@colibriserver.go.ro:/mnt/D/linicserver_backup_safe/
+pscp -pw %LINICSERVERPASS% %from4% linicserver@colibriserver.go.ro:/mnt/D/linicserver_backup_safe/
