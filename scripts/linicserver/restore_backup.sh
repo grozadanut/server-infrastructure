38837732f86b7b252bcbebddaf5a54bfc67bff15
Groza Danut <grozadanut95@gmail.com>
2025-02-21 12:51:09 +0200

    scp shortcut

--- scripts/linicserver/restore_backup.sh
+++ scripts/linicserver/restore_backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+scp gdany@colibriserver.go.ro:/mnt/D/linicserver_backup_safe/colibri_stat_db_2025-02-20_13-30.gz /home/danut
 gzip -k -d colibri_stat_db_2025-02-20_13-30.gz
 psql -U linic -d colibri_stat_db -f colibri_stat_db_2025-02-20_13-30 -h localhost
