d0813809c180a6217679ead140ab96aef62ebcb9
Groza Danut <groza_danut@yahoo.com>
2026-05-12 15:09:50 +0300

    fix: playwright tests

--- tests/security.spec.ts
+++ tests/security.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('login fail with non-existent user', async ({ page }) => {
-  await page.goto(`/`);
+  await page.goto(`/apps`);
   await page.getByRole('textbox', { name: 'Username' }).fill('notauser');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
@@ -9,7 +9,7 @@ test('login fail with non-existent user', async ({ page }) => {
 });
 
 test('login success with john.doe', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/apps');
   await page.getByRole('textbox', { name: 'Username' }).fill('john.doe');
   await page.getByRole('textbox', { name: 'Username' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
@@ -18,24 +18,24 @@ test('login success with john.doe', async ({ page }) => {
 });
 
 test('login success with 2FA: single use code', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('textbox', { name: 'Username' }).fill('sheldon.acme');
+  await page.goto('/apps');
+  await page.getByRole('textbox', { name: 'Username' }).fill('john.popc');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByRole('link', { name: ' My Account' }).click();
-  await page.locator('#user-panel-tabpanel').getByText('Account').click();
+  await page.getByRole('link', { name: 'My Account' }).click();
+  await page.getByRole('link', { name: 'Account', exact: true }).click();
   await page.getByRole('button', { name: 'Add Single Use Codes' }).click();
-  await page.getByRole('button', { name: 'Add', exact: true }).click();
-  var code = (await page.getByRole('listitem').textContent()) ?? '';
+  await page.locator('#CreateUserAuthcFactorSingleUse_submitButton').click();
+  const code = (await page.locator('.col-lg-4').first().textContent())?.trim() ?? '';
   await page.goto('/Login/logout');
-  await page.getByRole('textbox', { name: 'Username' }).fill('sheldon.acme');
+  await page.getByRole('textbox', { name: 'Username' }).fill('john.popc');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('textbox', { name: 'Authentication Code' }).click();
   await page.getByRole('textbox', { name: 'Authentication Code' }).fill(code);
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.goto('/Login/logout');
-  await page.getByRole('textbox', { name: 'Username' }).fill('sheldon.acme');
+  await page.getByRole('textbox', { name: 'Username' }).fill('john.popc');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await expect(page.getByText('Choose an Application' )).toBeVisible();
