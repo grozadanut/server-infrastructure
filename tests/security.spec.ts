a100274dc5f324bafa8bcaf7ee82dbeb03f8b6d5
Groza Danut <groza_danut@yahoo.com>
2026-05-25 17:10:53 +0300

    test(login): add playwright trustedDevice test case

--- tests/security.spec.ts
+++ tests/security.spec.ts
@@ -25,9 +25,12 @@ test('login success with 2FA: single use code', async ({ page }) => {
   await page.getByRole('link', { name: 'My Account' }).click();
   await page.getByRole('link', { name: 'Account', exact: true }).click();
   await page.getByRole('button', { name: 'Add Single Use Codes' }).click();
+  await page.getByLabel('Number of Codes').fill('2');
   await page.locator('#CreateUserAuthcFactorSingleUse_submitButton').click();
   const code = (await page.locator('.col-lg-4').first().textContent())?.trim() ?? '';
+  //const code2 = (await page.locator('.row > div:nth-child(2)').first().textContent())?.trim() ?? '';
   await page.goto('/Login/logout');
+  await page.goto('/apps');
   await page.getByRole('textbox', { name: 'Username' }).fill('john.doe');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
@@ -40,10 +43,16 @@ test('login success with 2FA: single use code', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign in' }).click();
   await expect(page.getByText('Choose an Application' )).toBeVisible();
   await page.goto('/Login/logout');
+  await page.goto('/apps');
+  // device should be remembered(trusted) for 30 days, so no 2FA should be required
   await page.getByRole('textbox', { name: 'Username' }).fill('john.doe');
   await page.getByRole('textbox', { name: 'Password' }).fill('moqui');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await expect(page.getByText('Choose an Application' )).toBeVisible();
+  // cleanup
+  await page.getByRole('link', { name: 'My Account' }).click();
+  await page.getByRole('link', { name: 'Account', exact: true }).click();
+  await page.locator('#UserAuthcFactorList_delete_0').click();
 });
 
 test('dont require email 2FA if not validated', async ({ page }) => {
